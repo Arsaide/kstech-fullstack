abde696dff49a423c1ebe308c473d08271b88880
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-16 18:33:41 +0300

    - changed next config

--- kstech-frontend/next.config.mjs
+++ kstech-frontend/next.config.mjs
@@ -13,9 +13,9 @@ const nextConfig = {
         ],
     },
     env: {
-        API_URL: 'https://kstech-backend.onrender.com',
+        // API_URL: 'https://kstech-backend.onrender.com',
         // API_URL: 'http://localhost:7000',
-        // API_URL: 'http://localhost/api',
+        API_URL: 'http://localhost/api',
         NGINX_API_URL: 'http://nginx/api'
     },
     httpAgentOptions: {
